207fb117716552f5ec2de3a739d21c3b7c88f4f9
fiologica <39060769+fiologica@users.noreply.github.com>
2024-03-16 21:25:51 +0000

    Had some glitches, made some tweaks

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 let TdP = 0
 let heatindexC = 0
+let tempC = 0
 let TempF = 0
 let humidityPercent = 0
 let HI = 0
 input.onButtonPressed(Button.A, function () {
     dewPoint()
     heatIndex()
+    basic.pause(100)
     display()
-    power.lowPowerEnable(LowPowerEnable.Allow)
 })
 function display () {
     kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(true))
@@ -16,17 +17,17 @@ function display () {
     kitronik_VIEW128x64.show("Pressure:" + " " + Kitronik_klimate.pressure(Kitronik_klimate.PressureUnitList.mBar) + " " + "mBar", 2)
     kitronik_VIEW128x64.show("Humidity:" + " " + Kitronik_klimate.humidity() + " " + "%", 3)
     kitronik_VIEW128x64.show("Dew" + " " + "point:" + " " + TdP + " " + "°C", 4)
-    kitronik_VIEW128x64.show("Heat" + " " + "index:" + "" + heatindexC + "" + "°C", 5)
+    kitronik_VIEW128x64.show("Heat" + " " + "index:" + " " + heatindexC + " " + "°C", 5)
     basic.pause(60000)
     kitronik_VIEW128x64.clear()
     kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(false))
 }
 input.onButtonPressed(Button.B, function () {
     kitronik_VIEW128x64.clear()
-    kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(false))
 })
 function heatIndex () {
-    if (Kitronik_klimate.temperature(Kitronik_klimate.TemperatureUnitList.C) >= 22) {
+    tempC = Kitronik_klimate.temperature(Kitronik_klimate.TemperatureUnitList.C)
+    if (tempC >= 22) {
         TempF = Kitronik_klimate.temperature(Kitronik_klimate.TemperatureUnitList.F)
         humidityPercent = Kitronik_klimate.humidity()
         HI = Math.trunc(0.5 * (TempF + 61 + (TempF - 68) * 1.2 + humidityPercent * 0.094))
